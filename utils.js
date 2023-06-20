7dc3097460a5a35d861a10158c3c1933d889a976
Justin Young <tempest.storm@gmail.com>
2023-06-20 19:27:38 -0400

    Added enum.js and redefined enums in utils.js

--- utils.js
+++ utils.js
@@ -1,37 +1,41 @@
+import { Enum } from './enum'
+
 // =====
 // enums
 // =====
 
 
 // product language
-const ProductLanguage = Object.freeze({
-    English: Symbol('ENG'),
-    Japanese: Symbol('JPN')
+const ProductLanguage = Enum({
+    English: 'ENG',
+    Japanese: 'JPN'
 });
 
 // product subtype
-const ProductSubType = Object.freeze({
-    Collector: Symbol('Collector'),
-    Draft: Symbol('Draft'),
-    FirstEdition: Symbol('1st Edition'),
-    Unlimited: Symbol('Unlimited'),
-    Set: Symbol('Set')
+const ProductSubType = Enum({
+    Collector: 'Collector',
+    Draft: 'Draft',
+    FirstEdition: '1st Edition',
+    Unlimited: 'Unlimited',
+    Set: 'Set'
 })
 
 // product type
-const ProductType = Object.freeze({
-    BoosterBox: Symbol('Booster Box'),
-    Bundle: Symbol('Bundle'),
-    CommanderDeck: Symbol('Commander Deck'),
-    EliteTrainerBox: Symbol('Elite Trainer Box'),
-    SecretLair: Symbol('Secret Lair)')
+const ProductType = Enum({
+    BoosterBox: 'Booster Box',
+    Bundle: 'Bundle',
+    CommanderDeck: 'Commander Deck',
+    EliteTrainerBox: 'Elite Trainer Box',
+    SecretLair: 'Secret Lair'
 });
 
 // TCG
-const TCG = Object.freeze({
-    FleshAndBlood: Symbol('Flesh and Blood'),
-    MagicTheGathering: Symbol('Magic: The Gathering'),
-    MetaZoo: Symbol('MetaZoo'),
-    Pokemon: Symbol('Pokemon'),
-    Sorcery: Symbol('Sorcery')
+const TCG = Enum({
+    FleshAndBlood: 'Flesh and Blood',
+    MagicTheGathering: 'Magic: The Gathering',
+    MetaZoo: 'MetaZoo',
+    Pokemon: 'Pokemon',
+    Sorcery: 'Sorcery'
 });
+
+module.exports = { ProductLanguage, ProductSubType, ProductType, TCG }
