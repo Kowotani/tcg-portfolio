cae40455613c3bb283f24c05f5a496bc763dcabc
Justin Young <tempest.storm@gmail.com>
2023-06-22 12:08:56 -0400

    Commented out isValidProductData() in utils.js since covered by productSchema

--- utils.js
+++ utils.js
@@ -45,78 +45,78 @@ const TCG = enumMod.Enum({
 // functions
 // =========
 
-/*
-DESC
-    Checks whether the input data object has correctly formatted data for
-    creating a new Product document:
+// /*
+// DESC
+//     Checks whether the input data object has correctly formatted data for
+//     creating a new Product document:
 
-    tcgplayer_id - the TCGplayer product id
-    release_date - product release date in YYYY-MM-DD format
-    name - product name
-    type - ProductType enum
-    language - ProductLanguage enum
-    subtype [OPTIONAL] - ProductSubType enum
-    set_code [OPTIONAL] - product set code
-INPUT
-    Object with data fields
-RETURN
-    TRUE if the input data object is valid for a Product doc, FALSE otherwise
-*/
-function isValidProductData(data) {
+//     tcgplayer_id - the TCGplayer product id
+//     release_date - product release date in YYYY-MM-DD format
+//     name - product name
+//     type - ProductType enum
+//     language - ProductLanguage enum
+//     subtype [OPTIONAL] - ProductSubType enum
+//     set_code [OPTIONAL] - product set code
+// INPUT
+//     Object with data fields
+// RETURN
+//     TRUE if the input data object is valid for a Product doc, FALSE otherwise
+// */
+// function isValidProductData(data) {
     
-    // tcgplayer_id
-    if (!('tcgplayer_id' in data)) {
-        throw new Error('[tcgplayer_id] property not found');
-    }
-    if (!Number.isInteger(Number(data.tcgplayer_id))) {
-        throw new Error('[tcgplayer_id] property is not an integer');
-    }
+//     // tcgplayer_id
+//     if (!('tcgplayer_id' in data)) {
+//         throw new Error('[tcgplayer_id] property not found');
+//     }
+//     if (!Number.isInteger(Number(data.tcgplayer_id))) {
+//         throw new Error('[tcgplayer_id] property is not an integer');
+//     }
 
-    // tcg
-    if (!('tcg' in data)) {
-        throw new Error('[tcg] property not found');
-    }
-    if (!Object.values(TCG).includes(data.tcg)) {
-        throw new Error(`Unrecognized TCG: ${data.tcg}`);
-    }    
+//     // tcg
+//     if (!('tcg' in data)) {
+//         throw new Error('[tcg] property not found');
+//     }
+//     if (!Object.values(TCG).includes(data.tcg)) {
+//         throw new Error(`Unrecognized TCG: ${data.tcg}`);
+//     }    
 
-    // release_date
-    if (!('release_date' in data)) {
-        throw new Error('[release_date] property not found');
-    }
-    try {
-        Date.parse(data.release_date.substring(0,10));
-    } catch(err) {
-        throw new Error(`Unrecognized format for release_date: ${date.release_date}`);
-    }
+//     // release_date
+//     if (!('release_date' in data)) {
+//         throw new Error('[release_date] property not found');
+//     }
+//     try {
+//         Date.parse(data.release_date.substring(0,10));
+//     } catch(err) {
+//         throw new Error(`Unrecognized format for release_date: ${date.release_date}`);
+//     }
 
-    // name
-    if (!('name' in data)) {
-        throw new Error('[name] property not found');
-    }
+//     // name
+//     if (!('name' in data)) {
+//         throw new Error('[name] property not found');
+//     }
 
-    // type
-    if (!('type' in data)) {
-        throw new Error('[type] property not found');
-    }
-    if (!Object.values(ProductType).includes(data.type)) {
-        throw new Error(`Unrecognized product type: ${data.type}`);
-    }
+//     // type
+//     if (!('type' in data)) {
+//         throw new Error('[type] property not found');
+//     }
+//     if (!Object.values(ProductType).includes(data.type)) {
+//         throw new Error(`Unrecognized product type: ${data.type}`);
+//     }
 
-    // language
-    if (!('language' in data)) {
-        throw new Error('[language] property not found');
-    }
-    if (!Object.values(ProductLanguage).includes(data.language)) {
-        throw new Error(`Unrecognized product language: ${data.language}`);
-    }
+//     // language
+//     if (!('language' in data)) {
+//         throw new Error('[language] property not found');
+//     }
+//     if (!Object.values(ProductLanguage).includes(data.language)) {
+//         throw new Error(`Unrecognized product language: ${data.language}`);
+//     }
 
-    // subtype
-    if (('subtype' in data) && !Object.values(ProductSubType).includes(data.subtype)) {
-        throw new Error(`Unrecognized product subtype: ${data.subtype}`);
-    }
-}
+//     // subtype
+//     if (('subtype' in data) && !Object.values(ProductSubType).includes(data.subtype)) {
+//         throw new Error(`Unrecognized product subtype: ${data.subtype}`);
+//     }
+// }
 
 module.exports = { 
-    ProductLanguage, ProductSubType, ProductType, TCG, isValidProductData 
+    ProductLanguage, ProductSubType, ProductType, TCG 
 }
