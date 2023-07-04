fe2655fa92c0cdcbac586228ee91221894b3c588
Justin Young <tempest.storm@gmail.com>
2023-07-04 16:21:43 -0400

    Added SelectInput component

--- tcg_portfolio/src/components/InputField.tsx
+++ tcg_portfolio/src/components/InputField.tsx
@@ -1,19 +1,18 @@
 import { FunctionComponent } from 'react';
 import { 
-    Box,
     Input, 
     InputGroup, 
     InputLeftAddon, 
     InputRightAddon,
-    Tooltip, 
-    VStack
+    Select,
+    Tooltip
 } from '@chakra-ui/react';
 
 
 // Error message tooltip
 interface ErrorTooltipProps {
     label: string,
-    children: JSX.Element,
+    children: JSX.Element | JSX.Element[],
     isOpen?: boolean,
 }
 const ErrorTooltip: FunctionComponent<ErrorTooltipProps> = ({
@@ -36,6 +35,7 @@ const ErrorTooltip: FunctionComponent<ErrorTooltipProps> = ({
     )
 }
 
+
 // Date input
 interface DateInputProps {
     value?: string
@@ -53,17 +53,6 @@ export const DateInput: FunctionComponent<DateInputProps> = ({
     isRequired = false,
     errorMessage = null,
 }) => {
-   
-    // const InputField = () => {
-    //     return (
-    //         <Input 
-    //             type='date'
-    //             isInvalid={isInvalid}
-    //             isRequired={isRequired} 
-    //             value={value} 
-    //         />
-    //     )
-    // }
 
     return (
         <InputGroup>
@@ -91,6 +80,61 @@ export const DateInput: FunctionComponent<DateInputProps> = ({
         </InputGroup>
     )
 }
+
+
+// Select input
+interface SelectInputProps {
+    placeholder: string,
+    values: string[],
+    leftLabel?: string,
+    rightLabel?: string,
+    isInvalid?: boolean,
+    isRequired?: boolean,
+    errorMessage?: string,
+}
+export const SelectInput: FunctionComponent<SelectInputProps> = ({
+    placeholder, 
+    values,
+    leftLabel = null, 
+    rightLabel = null, 
+    isInvalid = false, 
+    isRequired = false,
+    errorMessage = null,
+}) => {
+
+    const SelectComponent = (): JSX.Element => {
+        return (
+            <Select
+                isInvalid={isInvalid}
+                isRequired={isRequired} 
+                placeholder={placeholder} 
+            >            
+                {values.map(value => {
+                    return (
+                        <option value={value}>{value}</option>
+                    )})
+                }
+            </Select>
+        )
+    }
+
+    return (
+        <InputGroup>
+            {leftLabel ? <InputLeftAddon children={leftLabel} /> : null}
+            {errorMessage 
+                ? (
+                    <ErrorTooltip label={errorMessage} isOpen={isInvalid}>
+                        <SelectComponent />
+                    </ErrorTooltip>
+                ) : (
+                    <SelectComponent />
+                )
+            }
+            {rightLabel ? <InputRightAddon children={rightLabel} /> : null}
+        </InputGroup>
+    )
+}
+
 
 // Text input
 interface TextInputProps {
