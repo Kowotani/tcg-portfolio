a8c5838e5982e6939496b38a3b8528fea256daf9
Justin Young <tempest.storm@gmail.com>
2023-07-22 14:29:10 -0400

    Added FE utils.tsx with UserType and Nav enums

---
+++ tcg_portfolio/src/utils.tsx
@@ -0,0 +1,34 @@
+// =====
+// enums
+// =====
+
+// the types of user accounts
+export enum UserType {
+    Admin = 'Admin',
+    User = 'User'
+}
+
+// navigation SideBar links
+export const Nav = {
+    HOME: {
+        order: 1,
+        name: 'Home',
+        roles: [UserType.Admin, UserType.User]
+    },
+    PORTFOLIO: {
+        order: 2, 
+        name: 'Portfolio',
+        roles: [UserType.Admin, UserType.User]
+    },
+    PRODUCT: {
+        order: 3,
+        name: 'Product',
+        roles: [UserType.Admin, UserType.User]
+    },
+    ADD_PRODUCT: {
+        order: 4,
+        name: 'Add Product',
+        roles: [UserType.Admin]
+    }
+}
+
