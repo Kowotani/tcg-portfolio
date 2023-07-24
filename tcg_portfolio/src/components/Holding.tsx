af24f1cc7f587cc247e8a41ec8ada4060e03ba5c
Justin Young <tempest.storm@gmail.com>
2023-07-24 15:56:16 -0400

    Updated Holding component to use IHydratingHolding

--- tcg_portfolio/src/components/Holding.tsx
+++ tcg_portfolio/src/components/Holding.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { 
   Box,
+  Button,
   Card,
   CardBody,
   HStack,
@@ -9,30 +10,30 @@ import {
   Text,
   VStack
 } from '@chakra-ui/react';
-import { IHolding, IProduct } from 'common';
+import { IHydratedHolding, IProduct } from 'common';
 import { ProductDescription } from './ProductDescription';
 import { ProductImage } from './ProductImage';
 
 
+// ==============
+// Sub Components
+// ==============
+
+// summary metrics for the holding
 type THoldingSummaryProps = {
-  holding: IHolding
+  holding: IHydratedHolding
 }
 const HoldingSummary = (props: PropsWithChildren<THoldingSummaryProps>) => {
-
-  // TODO: Get BE to send these to FE
-  const quantity = 123
-  const cost = 456
-  const averageCost = 3.71
 
   return (
     // <Box display='flex' flexDirection='column' justifyContent='flex-end'>
     <SimpleGrid columns={2}>
       <Text align='left'>{`Quantity:`}</Text>
-      <Text align='right'>{`${quantity}`}</Text>
+      <Text align='right'>{`${props.holding.quantity}`}</Text>
       <Text align='left'>{`Cost:`}</Text>
-      <Text align='right'>{`$${cost}`}</Text>
+      <Text align='right'>{`$${props.holding.totalCost}`}</Text>
       <Text align='left'>{`Avg Cost:`}</Text>
-      <Text align='right'>{`$${averageCost}`}</Text>
+      <Text align='right'>{`$${props.holding.averageCost}`}</Text>
     </SimpleGrid>
   )
 }
@@ -43,7 +44,7 @@ const HoldingSummary = (props: PropsWithChildren<THoldingSummaryProps>) => {
 // ==============
 
 type THoldingCardProps = {
-  holding: IHolding,
+  holding: IHydratedHolding,
   product: IProduct
 }
 export const HoldingCard = (props: PropsWithChildren<THoldingCardProps>) => {
@@ -68,6 +69,7 @@ export const HoldingCard = (props: PropsWithChildren<THoldingCardProps>) => {
             >
               <ProductDescription product={props.product} hideHeader={true} />
               <HoldingSummary holding={props.holding} />
+              <Button colorScheme='blue'>Transactions</Button>
             </HStack>
           </VStack>
         </HStack>
