aea9bc6b35ae1f52b93bc1e631ffba10eb6f0241
Justin Young <tempest.storm@gmail.com>
2023-07-25 00:38:51 -0400

    Updated Holding component to use locale

--- tcg_portfolio/src/components/Holding.tsx
+++ tcg_portfolio/src/components/Holding.tsx
@@ -8,11 +8,14 @@ import {
   SimpleGrid,
   StackDivider,
   Text,
+  useDisclosure,
   VStack
 } from '@chakra-ui/react';
 import { IHydratedHolding, IProduct } from 'common';
+import { EditTransactionModal } from './EditTransactionModal';
 import { ProductDescription } from './ProductDescription';
 import { ProductImage } from './ProductImage';
+import { getBrowserLocale } from '../utils';
 
 
 // ==============
@@ -25,15 +28,22 @@ type THoldingSummaryProps = {
 }
 const HoldingSummary = (props: PropsWithChildren<THoldingSummaryProps>) => {
 
+  const locale = getBrowserLocale()
+
   return (
-    // <Box display='flex' flexDirection='column' justifyContent='flex-end'>
     <SimpleGrid columns={2}>
-      <Text align='left'>{`Quantity:`}</Text>
-      <Text align='right'>{`${props.holding.quantity}`}</Text>
-      <Text align='left'>{`Cost:`}</Text>
-      <Text align='right'>{`$${props.holding.totalCost}`}</Text>
-      <Text align='left'>{`Avg Cost:`}</Text>
-      <Text align='right'>{`$${props.holding.averageCost}`}</Text>
+      <Text align='left'>Quantity:</Text>
+      <Text align='right'>
+        {`${props.holding.quantity.toLocaleString(locale)}`}
+      </Text>
+      <Text align='left'>Total Cost:</Text>
+      <Text align='right'>
+        {`$${props.holding.totalCost.toLocaleString(locale)}`}
+      </Text>
+      <Text align='left'>Avg Cost:</Text>
+      <Text align='right'>
+        {`$${props.holding.averageCost.toLocaleString(locale)}`}
+      </Text>
     </SimpleGrid>
   )
 }
@@ -49,32 +59,42 @@ type THoldingCardProps = {
 }
 export const HoldingCard = (props: PropsWithChildren<THoldingCardProps>) => {
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Card>
-      <CardBody>
-        <HStack
-          divider={<StackDivider color='gray.200'/>}
-          spacing={4}
-        >
-          <ProductImage product={props.product} boxSize='100px'/>
-          <VStack spacing={0}>
-            <Box display='flex' justifyContent='flex-start' width='100%'>
-              <Text align='left' fontWeight='bold'>
-                {props.product.name}
-              </Text>
-            </Box>
-            <HStack
-              divider={<StackDivider color='gray.200'/>}
-              spacing={4}
-            >
-              <ProductDescription product={props.product} hideHeader={true} />
-              <HoldingSummary holding={props.holding} />
-              <Button colorScheme='blue'>Transactions</Button>
-            </HStack>
-          </VStack>
-        </HStack>
-      </CardBody>
-    </Card>
+    <>
+      <Card>
+        <CardBody>
+          <HStack
+            divider={<StackDivider color='gray.200'/>}
+            spacing={4}
+          >
+            <ProductImage product={props.product} boxSize='100px'/>
+            <VStack spacing={0}>
+              <Box display='flex' justifyContent='flex-start' width='100%'>
+                <Text align='left' fontWeight='bold'>
+                  {props.product.name}
+                </Text>
+              </Box>
+              <HStack
+                divider={<StackDivider color='gray.200'/>}
+                spacing={4}
+              >
+                <ProductDescription product={props.product} showHeader={false} />
+                <HoldingSummary holding={props.holding} />
+                <Button colorScheme='blue' onClick={onOpen}>Transactions</Button>
+              </HStack>
+            </VStack>
+          </HStack>
+        </CardBody>
+      </Card>
+      <EditTransactionModal 
+        isOpen={isOpen} 
+        onClose={onClose} 
+        product={props.product}
+        transactions={props.holding.transactions}
+      />
+    </>
   )
 
 }
