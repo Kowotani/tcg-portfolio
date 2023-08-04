c2d0eb538e43339a921ca0feaecde06fe40b72e7
Justin Young <tempest.storm@gmail.com>
2023-08-04 18:19:31 -0400

    Numerous updates to EditPortfolioForm, mostly for the SearchInput

--- tcg_portfolio/src/components/EditPortfolioForm.tsx
+++ tcg_portfolio/src/components/EditPortfolioForm.tsx
@@ -1,31 +1,25 @@
-import { PropsWithChildren, useContext, useEffect, useState, } from 'react';
-import axios from 'axios';
+import { PropsWithChildren, useContext, useEffect, useState, } from 'react'
+import axios from 'axios'
 import { 
   Box,
-  Button,
-  Image,
-  Icon,
   Input,
-  InputGroup,
-  InputLeftElement,
-  NumberInput,
-  NumberInputField,
-  Select,
-  useToast,
-  VStack, 
-} from '@chakra-ui/react';
-import { GET_PRODUCTS_URL, IHolding, IHydratedHolding, IPortfolio, IProduct, 
+} from '@chakra-ui/react'
+import { GET_PRODUCTS_URL, IHolding,  IPortfolio, IProduct, 
   isASCII, ITransaction, ProductLanguage, ProductSubtype, ProductType, TCG, 
-  TransactionType } from 'common';
+  TransactionType } from 'common'
 import { Field, FieldInputProps, Form, Formik, FormikHelpers, 
     FormikProps } from 'formik';
-import { HoldingCard } from './Holding';
-import { InputErrorWrapper } from './InputField';
-import { FaSearch } from 'react-icons/fa';
+import * as _ from 'lodash'
+import { HoldingCard } from './Holding'
+import { InputErrorWrapper } from './InputField'
+import { ProductSearchResult } from './ProductSearchResult';
+import { SearchInput } from './SearchInput'
 
-import { EditPortfolioContext } from '../state/EditPortfolioContext';
-import { SideBarNavContext } from '../state/SideBarNavContext';
-import { IEditPortfolioContext, ISideBarNavContext, SideBarNav } from '../utils';
+import { EditPortfolioContext } from '../state/EditPortfolioContext'
+import { SideBarNavContext } from '../state/SideBarNavContext'
+import { IEditPortfolioContext, ISideBarNavContext, NonVisibileProductSubtypes, 
+  SideBarNav } from '../utils'
+
 
 type TEditPortfolioProps = {}
 export const EditPortfolioForm = (
@@ -33,6 +27,8 @@ export const EditPortfolioForm = (
 ) => {
 
   // contexts
+  const { portfolio, setPortfolio} = 
+    useContext(EditPortfolioContext) as IEditPortfolioContext
   const { sideBarNav } = useContext(SideBarNavContext) as ISideBarNavContext
   const isEditPortfolioForm = sideBarNav === SideBarNav.PORTFOLIO
 
@@ -41,6 +37,10 @@ export const EditPortfolioForm = (
   // states
   // ======
 
+  // -----------------
+  // Portfolio Details
+  // -----------------
+
   // PortfolioName state
   const [ portfolioNameState, setPortfolioNameState ] = useState<{
     portfolioName: string | undefined,
@@ -48,15 +48,30 @@ export const EditPortfolioForm = (
     errorMessage?: string,
   }>({
     portfolioName: undefined,
-  });
+  })
 
-  // Search Product to Add
-  const [ addProductSearchValue, setAddProductSearchValue ] = useState('')
+  // --------------
+  // Product search
+  // --------------
+
+  // Search input
+  const [ searchInput, setSearchInput ] = useState('')
+
+  // Searchable products (that can be added)
+  const [ searchableProducts, setSearchableProducts ] =
+    useState([] as IProduct[])
+
+  // Search results
+  const [ searchResults, setSearchResults ] = useState([] as IProduct[])
 
 
   // =========
   // functions
   // =========
+
+  // ---------------
+  // form validation
+  // ---------------
 
   // validate PortfolioName
   function validatePortfolioName(input: string): void {
@@ -86,8 +101,35 @@ export const EditPortfolioForm = (
     }
   }
 
+  // --------------
+  // Product search
+  // --------------
+
+  function handleSearchChange(value: string): void {
+    _.debounce(() => setSearchInput(value), 300)
+  }
+  
   // ====================================
   // dummy data start
+
+  const fooTransactions: ITransaction[] = [
+    {
+      type: TransactionType.Purchase,
+      date: new Date(),
+      quantity: 123,
+      price: 5.67
+    }
+  ]
+
+  const fooHolding: IHolding = {
+    tcgplayerId: 121527,
+    transactions: fooTransactions
+  }
+  const fooPortfolio: IPortfolio = {
+    userId: 1234,
+    portfolioName: 'Alpha Investments',
+    holdings: [fooHolding]
+  }
 
   const fooProduct: IProduct = {
     tcgplayerId: 121527,
@@ -100,26 +142,68 @@ export const EditPortfolioForm = (
     setCode: 'KLD',
   }
 
-  const fooHolding: IHydratedHolding = {
-    tcgplayerId: 121527,
-    quantity: 1234,
-    price: 4,
-    totalCost: 456,
-    averageCost: 3.71,
-    marketValue: 492,
-    dollarReturn: 492 - 456,
-    percentageReturn: 492 / 456,
-    annualizedReturn: 0.69,
-    transactions: [{
-      type: TransactionType.Purchase,
-      date: new Date(),
-      quantity: 1234,
-      price: 5.67
-    }]
-  }
+  useEffect(() => {
+    setPortfolio(fooPortfolio)
+  }, [])
 
   // dummy data end
   // ====================================
+
+
+  // get initial products 
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: GET_PRODUCTS_URL,
+    })
+    .then(res => {
+      const data: IProduct[] = res.data.data
+      const existingTCGPlayerIds = portfolio.holdings.map(
+        holding => holding.tcgplayerId
+      )
+      const unheldProducts = data.filter(product => {
+        return !existingTCGPlayerIds.includes(product.tcgplayerId)
+      })
+      setSearchableProducts(unheldProducts)
+    })
+    .catch(err => {
+      console.log('Error fetching products: ' + err)
+    })
+  }, [])
+
+  // update Product seach results
+  useEffect(() => {
+    const productResults = searchableProducts
+      .filter(product => {    
+        return (
+          // name
+          _.toLower(product.name).match(_.toLower(searchInput)) !== null
+
+          // TCG
+          || _.toLower(product.tcg).match(_.toLower(searchInput)) !== null
+
+          // ProductType
+          || _.toLower(product.type).match(_.toLower(searchInput)) !== null
+
+          // ProductSubtype
+          || (_.toLower(product.subtype).match(_.toLower(searchInput)) !== null
+            && !NonVisibileProductSubtypes.includes(
+              product.subtype as ProductSubtype)
+            )
+        )
+      })
+      .sort((pA: IProduct, pB: IProduct): number => {
+        const valA = `${pA.name} ${pA.type} ${pA.subtype} ${pA.language}`
+        const valB = `${pB.name} ${pB.type} ${pB.subtype} ${pB.language}`
+        return valA > valB ? 1 : -1
+      })
+    setSearchResults(productResults)
+  }, [searchInput])
+
+
+  // ==============
+  // Main Component
+  // ==============
 
   return (
     <>
@@ -138,6 +222,7 @@ export const EditPortfolioForm = (
           <Input 
             isInvalid={portfolioNameState.isInvalid}
             onBlur={e => validatePortfolioName(e.target.value)}
+            defaultValue={portfolio.portfolioName}
             width='50%'
           />
         </InputErrorWrapper>
@@ -148,24 +233,22 @@ export const EditPortfolioForm = (
         Holdings
       </Box>      
 
-      {/* Search to add */}
-      <InputGroup 
-          display='flex' 
-          alignItems='center'
-          m='16px 0px'
-        >
-          <InputLeftElement 
-            pointerEvents='none'
-            children={<Icon as={FaSearch} color='gray.500' boxSize='18px'/>}
+      {/* Search Product to Add */}
+      <SearchInput 
+        placeholder='Product to Add'
+        maxSearchResults={5}
+        onSearchChange={e => setSearchInput(e.target.value)}
+        onSearchResultSelect={e => console.log(e)}
+        searchResultRenderer={(res: IProduct) => (
+          <ProductSearchResult 
+            product={res} 
+            searchInput={searchInput}
           />
-          <Input 
-            value={addProductSearchValue}
-            type='search' 
-            placeholder='Product to Add'
-            onChange={(e) => setAddProductSearchValue(e.target.value)}
-            width='50%'
-          />
-        </InputGroup>
+        )}
+        searchResults={searchResults}
+        searchResultKey='tcgplayerId'
+        value={searchInput}
+      />
 
       <HoldingCard 
         product={fooProduct}
