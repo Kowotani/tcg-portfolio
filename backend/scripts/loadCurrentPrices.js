3988e19d8e636dabd84e4d661cfb1f8080f9eebd
Justin Young <tempest.storm@gmail.com>
2023-09-22 15:35:35 -0400

    Fixed bug where the wrong variable was referenced

--- backend/scripts/loadCurrentPrices.js
+++ backend/scripts/loadCurrentPrices.js
@@ -3,7 +3,7 @@ const scrapeManager = require('../dist/scraper/scrapeManager')
 scrapeManager
   .loadCurrentPrices()
   .then(res => {
-    console.log(`${numInserted} price documents were loaded`) 
+    console.log(`${res} price documents were loaded`) 
   })
   .catch(err => {
     console.log(err)
