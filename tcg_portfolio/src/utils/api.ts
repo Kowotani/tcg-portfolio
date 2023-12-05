3e03efdade49d0edce87a8e9650b19d68a38ef01
Justin Young <tempest.storm@gmail.com>
2023-12-05 14:59:40 -0500

    Added FE /utils/api to handle parsing of endpoint responses

---
+++ tcg_portfolio/src/utils/api.ts
@@ -0,0 +1,69 @@
+import { 
+  IPopulatedHolding, IPopulatedPortfolio, IProduct, ITransaction, 
+
+  assert, getLocalDateFromISOString
+} from 'common'
+
+
+// =======
+// holding
+// =======
+
+
+// =========
+// portfolio
+// =========
+
+/*
+DESC
+  Parses the input response object from PORTFOLIOS_URL and returns an
+  IPopulatedPortfolio[]
+INPUT
+  portfolios: The response corresponding to an IPopulatedPortfolio[]
+RETURN
+  An IPopulatedPortfolio[]
+*/
+export function parsePortfoliosEndpointResponse(
+  response: any[]
+): IPopulatedPortfolio[] {
+
+  // parse populatedPortfolio
+  const portfolios = response.map((portfolio: any) => {
+
+    // parse populatedHoldings
+    const holdings = portfolio.populatedHoldings.map((holding: any) => {
+      
+      // parse product
+      const product = {
+        ...holding.product,
+        releaseDate: getLocalDateFromISOString(holding.product.releaseDate)
+      } as IProduct
+
+      // parse transactions
+      const transactions = holding.transactions.map((transaction: any) => {
+        return {
+          ...transaction,
+          date: getLocalDateFromISOString(transaction.date)
+        } as ITransaction
+      })
+
+      return {
+        product: product,
+        transactions: transactions
+      } as IPopulatedHolding
+    })
+
+    return {
+      userId: portfolio.userId,
+      description: portfolio.description,
+      populatedHoldings: holdings
+    } as IPopulatedPortfolio
+  }) 
+
+  return portfolios as IPopulatedPortfolio[]
+}
+
+
+// =======
+// product
+// =======
