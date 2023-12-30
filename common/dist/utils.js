b7c78500200efe03ac0b2a2c3551e582548097d4
Justin Young <tempest.storm@gmail.com>
2023-12-30 14:43:12 -0500

    Added sortDatedValues() to common utils

--- common/dist/utils.js
+++ common/dist/utils.js
@@ -1,7 +1,8 @@
 "use strict";
 exports.__esModule = true;
-exports.sortFnDateDesc = exports.sortFnDateAsc = exports.sleep = exports.logObject = exports.isTCGPriceTypeValue = exports.isPriceString = exports.isASCII = exports.getProductSubtypes = exports.getPriceFromString = exports.assert = exports.SECONDS_PER_DAY = exports.MILLISECONDS_PER_SECOND = exports.DAYS_PER_YEAR = void 0;
+exports.sortFnDatedValueDesc = exports.sortFnDatedValueAsc = exports.sortFnDateDesc = exports.sortFnDateAsc = exports.sortDatedValues = exports.sleep = exports.logObject = exports.isTCGPriceTypeValue = exports.isPriceString = exports.isASCII = exports.getValueAtDate = exports.getProductSubtypes = exports.getPriceFromString = exports.assert = exports.SECONDS_PER_DAY = exports.MILLISECONDS_PER_SECOND = exports.DAYS_PER_YEAR = void 0;
 var dataModels_1 = require("./dataModels");
+var dateUtils_1 = require("./dateUtils");
 var _ = require("lodash");
 var util_1 = require("util");
 // =========
@@ -59,6 +60,64 @@ function getProductSubtypes(tcg, productType) {
     return _.intersection(tcgArray, productTypeArray);
 }
 exports.getProductSubtypes = getProductSubtypes;
+/*
+DESC
+  Returns the value from the input TDatedValue[] at the input Date (optionally
+    return the closest value found)
+INPUT
+  datedValues: A TDatedValue[]
+  date: The Date to find
+  closest?: Whether to return the closest value if an exact date match is not
+    found (default: FALSE)
+  useEarlier?: In the event of a tie for closest, whether to use the value
+    corresponding to the earlier date (default: TRUE)
+RETURN
+  The value corresponding to the (closest) date, or potentially undefined if
+  there is no match
+*/
+function getValueAtDate(datedValues, date, closest, useEarlier) {
+    // sort datedValues
+    var sortedValues = sortDatedValues(datedValues);
+    // look for exact match
+    var exactValues = sortedValues.filter(function (dv) {
+        return (0, dateUtils_1.areDatesEqual)(dv.date, date);
+    });
+    // datedValues are empty
+    if (datedValues.length === 0) {
+        return undefined;
+        // exact match found
+    }
+    else if (exactValues.length > 0) {
+        return _.first(exactValues).value;
+        // find closest value
+    }
+    else if (closest) {
+        // date is before earliest date
+        if ((0, dateUtils_1.isDateBefore)(date, _.first(sortedValues).date)) {
+            return _.first(sortedValues).value;
+            // date is after latest date
+        }
+        else if ((0, dateUtils_1.isDateAfter)(date, _.last(sortedValues).date)) {
+            return _.last(sortedValues).value;
+            // date is between other dates
+        }
+        else {
+            var earlier = _.last(sortedValues.filter(function (dv) { return dv.date < date; }));
+            var earlierDiff = (0, dateUtils_1.dateDiff)(earlier.date, date, 'days');
+            var later = _.first(sortedValues.filter(function (dv) { return dv.date > date; }));
+            var laterDiff = (0, dateUtils_1.dateDiff)(date, later.date, 'days');
+            return earlierDiff < laterDiff
+                || (earlierDiff === laterDiff && useEarlier)
+                ? earlier.value
+                : later.value;
+        }
+        // return undefined
+    }
+    else {
+        return undefined;
+    }
+}
+exports.getValueAtDate = getValueAtDate;
 /*
 DESC
   Returns whether the input string contains only ASCII characters
@@ -126,6 +185,19 @@ exports.sleep = sleep;
 // -------
 /*
 DESC
+  Returns the input TDatedValue[] after sorting (ascending or descending)
+INPUT
+  datedValues: A TDatedValue[]
+  desc?: Whether to sort in descending order (default: FALSE)
+RETURN
+  The sorted TDatedValue[]
+*/
+function sortDatedValues(datedValues, desc) {
+    return datedValues.sort(desc ? sortFnDatedValueDesc : sortFnDatedValueAsc);
+}
+exports.sortDatedValues = sortDatedValues;
+/*
+DESC
   Function used for sorting dates in ascending order
 INPUT
   a: The first Date
@@ -150,3 +222,29 @@ function sortFnDateDesc(a, b) {
     return b.getTime() - a.getTime();
 }
 exports.sortFnDateDesc = sortFnDateDesc;
+/*
+DESC
+  Function used for sorting TDatedValues in ascending order
+INPUT
+  a: The first TDatedValue
+  b: The second TDatedValue
+RETURN
+  A negative number if a < b, otherwise a positive number if a > b
+*/
+function sortFnDatedValueAsc(a, b) {
+    return sortFnDateAsc(a.date, b.date);
+}
+exports.sortFnDatedValueAsc = sortFnDatedValueAsc;
+/*
+DESC
+  Function used for sorting TDatedValues in descending order
+INPUT
+  a: The first TDatedValue
+  b: The second Date
+RETURN
+  A negative number if a > b, otherwise a positive number if a < b
+*/
+function sortFnDatedValueDesc(a, b) {
+    return sortFnDateDesc(a.date, b.date);
+}
+exports.sortFnDatedValueDesc = sortFnDatedValueDesc;
--- common/utils.ts
+++ common/utils.ts
@@ -1,8 +1,9 @@
 import {
-  ProductSubtype, ProductType, TCG, TCGPriceType,
+  ProductSubtype, ProductType, TCG, TCGPriceType, TDatedValue,
 
   ProductTypeToProductSubtype, TCGToProductSubtype
 } from './dataModels'
+import { areDatesEqual, dateDiff, isDateAfter, isDateBefore } from './dateUtils'
 import * as _ from 'lodash'
 import { inspect } from 'util'
 
@@ -61,10 +62,86 @@ INPUT
 RETURN
   An array of ProductSubtypes for the given TCG and ProductType
 */
-export function getProductSubtypes(tcg: TCG, productType: ProductType): ProductSubtype[] {
+export function getProductSubtypes(
+  tcg: TCG, 
+  productType: ProductType
+): ProductSubtype[] {
   const tcgArray = TCGToProductSubtype[tcg];
   const productTypeArray = ProductTypeToProductSubtype[productType];
   return _.intersection(tcgArray, productTypeArray);
+}
+
+/*
+DESC
+  Returns the value from the input TDatedValue[] at the input Date (optionally 
+    return the closest value found)
+INPUT
+  datedValues: A TDatedValue[]
+  date: The Date to find
+  closest?: Whether to return the closest value if an exact date match is not 
+    found (default: FALSE)
+  useEarlier?: In the event of a tie for closest, whether to use the value 
+    corresponding to the earlier date (default: TRUE)
+RETURN
+  The value corresponding to the (closest) date, or potentially undefined if
+  there is no match
+*/
+export function getValueAtDate(
+  datedValues: TDatedValue[],
+  date: Date,
+  closest?: boolean,
+  useEarlier?: boolean
+): number | undefined {
+
+  // sort datedValues
+  const sortedValues = sortDatedValues(datedValues)
+
+  // look for exact match
+  const exactValues = sortedValues.filter((dv: TDatedValue) => {
+    return areDatesEqual(dv.date, date)
+  })
+
+  // datedValues are empty
+  if (datedValues.length === 0) {
+    return undefined
+
+  // exact match found
+  } else if (exactValues.length > 0) {
+    return _.first(exactValues).value
+  
+  // find closest value
+  } else if (closest) {
+
+    // date is before earliest date
+    if (isDateBefore(date, _.first(sortedValues).date)) {
+      return _.first(sortedValues).value
+    
+    // date is after latest date
+    } else if (isDateAfter(date, _.last(sortedValues).date)) {
+      return _.last(sortedValues).value
+
+    // date is between other dates
+    } else {
+
+      const earlier = _.last(
+        sortedValues.filter((dv: TDatedValue) => { return dv.date < date })
+      )
+      const earlierDiff = dateDiff(earlier.date, date, 'days')
+      const later = _.first(
+        sortedValues.filter((dv: TDatedValue) => { return dv.date > date })
+      )
+      const laterDiff = dateDiff(date, later.date, 'days')
+
+      return earlierDiff < laterDiff 
+        || (earlierDiff === laterDiff && useEarlier)
+          ? earlier.value
+          : later.value 
+    }
+
+  // return undefined
+  } else {
+    return undefined
+  }
 }
 
 /*
@@ -128,9 +205,26 @@ export function sleep(ms: number): Promise<any> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+
 // -------
 // sorting
 // -------
+
+/*
+DESC
+  Returns the input TDatedValue[] after sorting (ascending or descending)
+INPUT
+  datedValues: A TDatedValue[]
+  desc?: Whether to sort in descending order (default: FALSE)
+RETURN
+  The sorted TDatedValue[]
+*/
+export function sortDatedValues(
+  datedValues: TDatedValue[],
+  desc?: boolean
+): TDatedValue[] {
+  return datedValues.sort(desc ? sortFnDatedValueDesc : sortFnDatedValueAsc)
+}
 
 /*
 DESC
@@ -156,4 +250,30 @@ RETURN
 */
 export function sortFnDateDesc(a: Date, b: Date): number {
   return b.getTime() - a.getTime()
+}
+
+/*
+DESC
+  Function used for sorting TDatedValues in ascending order
+INPUT
+  a: The first TDatedValue
+  b: The second TDatedValue
+RETURN
+  A negative number if a < b, otherwise a positive number if a > b
+*/
+export function sortFnDatedValueAsc(a: TDatedValue, b: TDatedValue): number {
+  return sortFnDateAsc(a.date, b.date)
+}
+
+/*
+DESC
+  Function used for sorting TDatedValues in descending order
+INPUT
+  a: The first TDatedValue
+  b: The second Date
+RETURN
+  A negative number if a > b, otherwise a positive number if a < b
+*/
+export function sortFnDatedValueDesc(a: TDatedValue, b: TDatedValue): number {
+  return sortFnDateDesc(a.date, b.date)
 }
