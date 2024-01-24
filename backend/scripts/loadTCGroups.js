19f089b1c96e874e12fbc323f56ed75473366cd7
Justin Young <tempest.storm@gmail.com>
2024-01-23 22:29:09 -0500

    Updated logging messages in loadTCGroups.js

--- backend/scripts/loadTCGroups.js
+++ backend/scripts/loadTCGroups.js
@@ -2,11 +2,11 @@ const scrapeManager = require('../dist/scraper/scrapeManager')
 const tcgcsvUtils = require('../dist/utils/tcgcsv')
 
 tcgcsvUtils.TCG_TO_TCCATEGORY.forEach(async (value, key) => {
-  console.log(`Scraping TCG [${key}, ${value.categoryId}]...`)
+  console.log(`Scraping Groupos for TCG [${key}, ${value.categoryId}]...`)
   await scrapeManager
     .loadTCGroups(value.categoryId)
     .then(res => {
-      console.log(`${res} Group documents were loaded for ${key}`)
+      console.log(`${res} TCGroup documents were loaded for ${key}`)
     })
     .catch(err => {
       console.log(err)  
